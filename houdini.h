c7e9df4b78e3757fa3057c8ce0d1c83860fff679
Vicent Marti <tanoku@gmail.com>
2011-08-31 02:26:59 +0200

    Update header

--- houdini.h
+++ houdini.h
@@ -3,10 +3,11 @@
 
 #include "buffer.h"
 
-extern void
-houdini_escape_html(struct buf *ob, const char *src, size_t size);
-
-extern void
-houdini_unescape_html(struct buf *ob, const char *src, size_t size);
+extern void houdini_escape_html(struct buf *ob, const char *src, size_t size);
+extern void houdini_unescape_html(struct buf *ob, const char *src, size_t size);
+extern void houdini_escape_uri(struct buf *ob, const char *src, size_t size);
+extern void houdini_escape_url(struct buf *ob, const char *src, size_t size);
+extern void houdini_unescape_uri(struct buf *ob, const char *src, size_t size);
+extern void houdini_unescape_url(struct buf *ob, const char *src, size_t size);
 
 #endif
