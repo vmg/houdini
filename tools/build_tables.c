80c92a5f75abe4217c41531b848251620bd8b68e
Vicent Marti <tanoku@gmail.com>
2011-09-13 00:17:05 +0200

    Add `houdini_escape_href`
    Does mixed URL/HTML escaping for URLs inside an HTML tag.

--- tools/build_tables.c
+++ tools/build_tables.c
@@ -8,9 +8,22 @@
 #define RESERVED(c) (c == ';' || c == '/' || c == '?' || c == ':' || c == '@' || c== '&' || c == '=' || c == '+' || c == '$' || c == ',' || c == '[' || c == ']')
 #define URI_SAFE(c) (URL_SAFE(c) || UNRESERVED(c) || RESERVED(c))
 
+#define HREF_HTML_SAFE(c) (URL_SAFE(c) || strchr("-_.+!*(),%#@?=;:/,+$", c) != NULL)
+
 int main(void)
 {
 	int i;
+
+	printf("static const char HREF_SAFE[] = {\n\t");
+
+	for (i = 0; i < 256; ++i) {
+		printf("%d, ", HREF_HTML_SAFE(i));
+
+		if (i % 16 == 15 && i < 255)
+			printf("\n\t");
+	}
+
+	printf("};\n\n");
 
 	printf("static const char URL_SAFE[] = {\n\t");
 
